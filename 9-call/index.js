f8e7e771baea53a6d1cca31b14dfa4c2921666c0
thinhtpt2000 <tthinh2128@gmail.com>
2020-07-20 20:29:49 +0700

    10th: call - using

---
+++ 9-call/index.js
@@ -0,0 +1,15 @@
+// Call
+
+function greeting() {
+	console.log(`Hi, ${this.name}! I am ${this.age}.`);
+}
+var user = {
+	name: 'Thinh',
+	age: 20
+}
+greeting.call(user);
+
+/*
+* Khác với bind một tí, bind sẽ trả về một hàm và hàm đấy phải thêm () để chạy (xem lại bài 3)
+* Call thì gọi hàm đấy và trả về giá trị mà hàm trả về
+*/
