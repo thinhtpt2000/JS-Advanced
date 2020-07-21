aabc2fbdd1fd9a70fe1906cdd89afdea2d3e982b
thinhtpt2000 <tthinh2128@gmail.com>
2020-07-21 20:25:38 +0700

    14th: class

---
+++ 12-class/index.js
@@ -0,0 +1,22 @@
+// Constructor function
+/*
+function Mouse(name) {
+	this.name = name;
+}
+Mouse.prototype.run = function() {
+	console.log(`${this.name} is running`);
+};
+*/
+
+class Mouse {
+	constructor(name) {
+		this.name = name;
+	}
+
+	run() {
+		console.log(`${this.name} is running`);
+	}
+}
+
+const mouse = new Mouse('Mickey');
+mouse.run();
